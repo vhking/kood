449fa3b70dedc5e9cd7c0eddfdba68939dab3c1d
vhking <vidarheltne@hotmail.no>
2022-04-15 15:22:43 +0200

    correct 64bit int define

--- code/main.cpp
+++ code/main.cpp
@@ -8,8 +8,8 @@
 typedef unsigned char c8;
 typedef unsigned int u32;
 typedef signed	 int i32;
-typedef unsigned long int u64;
-typedef signed	 long int i64;
+typedef unsigned long long u64;
+typedef signed	 long long i64;
 
 struct String {
 	c8 	*base;
